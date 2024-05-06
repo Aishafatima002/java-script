79f4754ef4000264f5a839d614ae4bc44b8787fe
Aisha <164768280+Aishafatima002@users.noreply.github.com>
2024-05-06 16:32:43 +0500

    simple js

---
+++ script.js
@@ -0,0 +1,59 @@
+// var dateofbirth;//declare
+// dateofbirth;//intialize
+// console.log(dateofbirth);
+// var name="Aisha fatima";
+// var age=18;
+// console.log("my name is"+ name+"my age is"+age);
+// var name="aisha fatima";
+// alert(name);
+// let x=12;
+// let y=3;
+// // let result=x*y;
+// console.log("the addition of two numbers"+ (x+y));
+// console.log("the multiplicatios two numbers"+(x*y))
+// console.log("the subracction of two number"+(x-y))
+// console.log("the modulus of two number"+(x%y))
+
+//legal variable
+// let firstname;
+// let lastname;
+// let _dateofbirth;
+// let $email;
+
+//illegal variable
+// let 12;
+// let 12return;
+// let !hello;
+// let@ myvar;
+// let "aisha";
+
+// let rose="flower";
+// console.log(Rose);
+
+// //questions no1
+// let onevar="one";secondvar="second";thirdvar="third";
+
+// // question2
+// //legal variables
+// let firstname="aisha"
+// let lastname="fatima"
+
+
+// //illegal variables
+// let 12;
+// let "name";
+// let @Email;
+
+
+//question no3 n4
+let x=5;
+let y=10;
+console.log("the addition of two numbers"+ (x+y));
+console.log("the multiplicatios two numbers"+(x*y));
+console.log("the subracction of two number"+(x-y));
+console.log("the modulus of two number"+(x%y));
+//question no5
+let costofticket=600;
+let buyingticket=5;
+let result="costofticket+buyingticket";
+console.log("total cost"+(costofticket* buyingticket))
